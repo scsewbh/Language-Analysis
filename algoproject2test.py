49acc65609e4850a3a84892a575f9ca5f82543c2
Eric <ericciccotelli@gmail.com>
2021-10-14 20:43:40 -0400

    Initial commit of test example

---
+++ algoproject2test.py
@@ -0,0 +1,61 @@
+from re import sub
+from gensim.utils import simple_preprocess
+import numpy as np
+
+query_string = 'planets'
+documents = ['mars', 'fruit']
+
+query_string = 'Convalescence after chemotherapy (finding)'
+
+documents = ['Convalescence after radiotherapy (finding)', 'Convalescence after psychotherapy (finding)']
+
+stopwords = ['the', 'and', 'are', 'a']
+
+# From: https://github.com/RaRe-Technologies/gensim/blob/develop/docs/notebooks/soft_cosine_tutorial.ipynb
+def preprocess(doc):
+    # Tokenize, clean up input document string
+    doc = sub(r'<img[^<>]+(>|$)', " image_token ", doc)
+    doc = sub(r'<[^<>]+(>|$)', " ", doc)
+    doc = sub(r'\[img_assist[^]]*?\]', " ", doc)
+    doc = sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', " url_token ", doc)
+    return [token for token in simple_preprocess(doc, min_len=0, max_len=float("inf")) if token not in stopwords]
+
+# Preprocess the documents, including the query string
+corpus = [preprocess(document) for document in documents]
+query = preprocess(query_string)
+
+import gensim.downloader as api
+from gensim.corpora import Dictionary
+from gensim.models import TfidfModel
+from gensim.similarities import WordEmbeddingSimilarityIndex
+from gensim.similarities import SparseTermSimilarityMatrix
+from gensim.similarities import SoftCosineSimilarity
+
+# Load the model: this is a big file, can take a while to download and open
+glove = api.load("glove-wiki-gigaword-50")    
+similarity_index = WordEmbeddingSimilarityIndex(glove)
+
+# Build the term dictionary, TF-idf model
+dictionary = Dictionary(corpus+[query])
+tfidf = TfidfModel(dictionary=dictionary)
+
+# Create the term similarity matrix.  
+similarity_matrix = SparseTermSimilarityMatrix(similarity_index, dictionary, tfidf)
+
+# Compute Soft Cosine Measure between the query and the documents.
+# From: https://github.com/RaRe-Technologies/gensim/blob/develop/docs/notebooks/soft_cosine_tutorial.ipynb
+query_tf = tfidf[dictionary.doc2bow(query)]
+
+index = SoftCosineSimilarity(
+            tfidf[[dictionary.doc2bow(document) for document in corpus]],
+            similarity_matrix)
+
+doc_similarity_scores = index[query_tf]
+
+# Output the sorted similarity scores and documents
+sorted_indexes = np.argsort(doc_similarity_scores)[::-1]
+for idx in sorted_indexes:
+    print(f'{idx} \t {doc_similarity_scores[idx]:0.3f} \t {documents[idx]}')
+
+# 1    0.688    tomatoes are actually fruit
+# 0    0.000    cars drive on the road
